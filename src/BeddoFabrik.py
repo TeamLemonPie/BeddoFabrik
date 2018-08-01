3a8f65207abc67a2921f0faa9adcf21c30dc7c4d
Robert Goldmann <deadlocker@gmx.de>
2018-08-01 17:02:48 +0200

    Fixed #21 - automatic network discovery; massive refactoring

---
+++ src/BeddoFabrik.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from sqlite3 import Connection
+
+import MFRC522
+import signal
+import time
+import json
+from Cards import Cards
+from ManageCards import ManageCards
+from ServerDiscover import ServerDiscover
+from Settings import Settings
+from Connection import Connection
+from thread import start_new_thread
+from Logger import Logger
+
+class BeddoFabrik:
+	def __init__(self):
+		self._continueReading = True
+		self._settings = None
+		self._readers = []
+		self._beddoMischerIP = None
+		self._connection = None
+
+	def Start(self):
+		# hook the SIGINT
+		signal.signal(signal.SIGINT, self.__EndRead)
+
+		# load settings
+		self._settings = Settings()
+		self._readers = self._readers
+		Logger.debug("{} devices registered:".format(len(self._readers)))
+		for x in self._readers:
+			Logger.debug(x)
+
+		# discover BeddoMischer IP
+		discoverer = ServerDiscover()
+		self._beddoMischerIP = discoverer.Discover()
+
+		# connect with server
+		self.__Connect()
+
+		# start thread to listen for incoming clear requests
+		start_new_thread(self.__ListenForClearRequests, ())
+
+		self.__MainLoop()
+
+	# Capture SIGINT for cleanup when the script is aborted
+	def __EndRead(self, signal, frame):
+		Logger.info("Ctrl+C captured, ending read.")
+		self._continueReading = False
+		GPIO.cleanup()
+		self._connection.active = False
+		self._connection.close()
+
+	def __Connect(self):
+		if self._connection is None or not self._connection.active:
+			self._connection = Connection()
+			self._connection.connect(self._beddoMischerIP, self._settings.server["port"])
+
+	def __ListenForClearRequests(self):
+		while True:
+			try:
+				requestData = self._connection.read()
+				self.__HandleClearRequest(requestData.strip())
+			except:
+				self.__Connect()
+
+	def __HandleClearRequest(self, requestData):
+		if requestData == "":
+			return
+
+		requestData = requestData.split("\n")
+		for line in requestData:
+			try:
+				obj = json.loads(line)
+				key = obj["key"]
+				Logger.debug("Received Clear: {}".format(key))
+				if key == -1:
+					self.__ClearAllHolds()
+				else:
+					self.__ClearHold(key)
+			except:
+				Logger.error("Error while parsing JSON. Data: {}".format(line))
+
+	def __ClearAllHolds(self):
+		for reader in self._readers:
+			reader.clearHold()
+
+	def __ClearHold(self, readerID):
+		for reader in self._readers:
+			if reader.id == readerID:
+				reader.clearHold()
+
+	def __MainLoop(self):
+		while self._continueReading:
+			for currentReader in self._readers:
+				if not currentReader.activated:
+					continue
+
+				reader = MFRC522.MFRC522(bus=currentReader.bus,
+										 subbus=currentReader.subbus,
+										 reset=currentReader.reset)
+
+				self.__HandleReader(reader, currentReader)
+
+				reader.Close()
+				time.sleep(0.2)
+
+
+	def __HandleReader(self, reader, currentReader):
+		(_, TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
+		(status, uid) = reader.MFRC522_Anticoll()
+
+		if status != reader.MI_OK:
+			return
+
+		uid = "-".join(map(str, uid))
+
+		# check if detected card is manage card
+		if self.__HandleManageCard(uid, currentReader):
+			return
+
+		self.__HandleNormalCard(uid, currentReader)
+
+	def __HandleManageCard(self, uid, currentReader):
+		manageCards = ManageCards()
+		manageCard = manageCards.getCardFromUID(uid)
+
+		if manageCard is None:
+			return False
+
+		Logger.debug("Reader {} detected manage card: {}".format(currentReader.id, manageCard))
+
+		dataDict = {
+			"scope": "READER",
+			"command": "manageCard",
+			"key": currentReader.id,
+			"value": manageCard
+		}
+
+		data = json.dumps(dataDict)
+		data += "\r\n"
+		if not self._connection.send(data):
+			Logger.error("Error while sending manage card to server")
+			self.__Connect()
+
+		return True
+
+	def __HandleNormalCard(self, uid, currentReader):
+		if not currentReader.isNewCard(uid):
+			Logger.debug("Skipping detected card for reader {}".format(currentReader.id))
+			return
+
+		cards = Cards()
+		card = cards.getCardFromUID(uid)
+		if card is None:
+			return
+
+		Logger.debug("Reader {} detected new card: {}".format(currentReader.id, card))
+
+		dataDict = {
+			"scope": "READER",
+			"command": "card",
+			"key": currentReader.id,
+			"value": card
+		}
+
+		data = json.dumps(dataDict)
+		data += "\r\n"
+		if not self._connection.send(data):
+			Logger.error("Error while sending new card to server")
+			self.__Connect()
+			# card could not be sent due to connection issues
+			currentReader.clearCardFromHold(uid)
---
+++ src/ServerDiscover.py
@@ -0,0 +1,28 @@
+import socket
+from Logger import Logger
+
+class ServerDiscover:
+	BROADCAST_IP = "255.255.255.255"
+	DISCOVER_PORT = 9990
+	DISCOVER_RESPONSE_PORT = 9991
+	DISCOVER_REQUEST = "DISCOVER_BEDDOMISCHER_REQUEST"
+	DISCOVER_RESPONSE = "DISCOVER_BEDDOMISCHER_RESPONSE"
+
+	def __init__(self):
+		pass
+
+	def Discover(self):
+		Logger.info("Trying to discover BeddoMischer...")
+		ip = self.__SendRequest()
+		Logger.info("Found BeddoMischer with IP: {}".format(ip))
+		return ip
+
+	def __SendRequest(self):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+		sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+		sock.sendto(self.DISCOVER_REQUEST.encode(), ("<broadcast>", self.DISCOVER_PORT))
+
+		while True:
+			data, ip = sock.recvfrom(1024)
+			if data.decode() == self.DISCOVER_RESPONSE:
+				return ip[0]
